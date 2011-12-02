a654fd12fe8d706615400984695f909cf41d1099
Matthew Astley <mca1001@users.sourceforge.net>
2011-12-02 22:26:28 +0000

    Import of MCAST/App-psst-0.09 from CPAN.
    gitpan-cpan-distribution: App-psst
    gitpan-cpan-version:      0.09
    gitpan-cpan-path:         MCAST/App-psst-0.09.tar.gz
    gitpan-cpan-author:       MCAST
    gitpan-cpan-maturity:     released

--- lib/App/psst.pm
+++ lib/App/psst.pm
@@ -3,7 +3,7 @@ use warnings;
 
 package App::psst;
 {
-  $App::psst::VERSION = '0.08';
+  $App::psst::VERSION = '0.09';
 }
 
 
@@ -14,7 +14,7 @@ App::psst - prompt string setting tool
 
 =head1 VERSION
 
-version 0.08
+version 0.09
 
 =head1 DESCRIPTION
 
